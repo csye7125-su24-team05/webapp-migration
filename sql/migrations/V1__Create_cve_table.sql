b2f7c671e6ad264d8fcd07752fef933908886ed6
Nexflare <harshmagarwal@outlook.com>
2024-06-10 20:21:45 -0400

    BREAKING CHANGE: SQL migration version 1

---
+++ sql/migrations/V1__Create_cve_table.sql
@@ -0,0 +1,15 @@
+-- cve schema
+
+CREATE TABLE cve.cve_records (
+    id SERIAL PRIMARY KEY,
+    cve_id VARCHAR(255) NOT NULL,
+    cve_data JSONB NOT NULL,
+    version INT NOT NULL,
+    timestamp TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE(cve_id, version)
+);
+
+-- GIN = Generalized Inverted Index (index in PostgreSQL designed to handle indexing for complex data types like jsonb, array, etc.)
+
+CREATE INDEX idx_cve_id ON cve.cve_records (cve_id);
+CREATE INDEX idx_cve_data ON cve.cve_records USING GIN (cve_data);
